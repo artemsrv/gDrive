53dc48e6663defca42721fb39a31b4dacd4d9935
Артем Сидоров <artemsidorov@MacBook-Air-Artem.local>
2016-12-11 13:48:34 +0300

    Initial Commit

---
+++ gDriveApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gDriveApp
+//
+//  Created by Артем Сидоров on 11.12.16.
+//  Copyright © 2016 artemosman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gDriveApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gDriveApp
+//
+//  Created by Артем Сидоров on 11.12.16.
+//  Copyright © 2016 artemosman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
