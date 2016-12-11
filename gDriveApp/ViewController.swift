0b0effd720f33e504aaba6a026db48d19d761312
Артем Сидоров <artemsidorov@MacBook-Air-Artem.local>
2016-12-12 02:48:41 +0300

    FileDialog added

--- gDriveApp/ViewController.swift
+++ gDriveApp/ViewController.swift
@@ -9,19 +9,54 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var textBox: NSTextField!
 
+    @IBOutlet weak var textField: NSTextField!
+        
+    @IBOutlet weak var filename: NSTextField!
+    
+    // show file system
+    @IBAction func showDir(_ sender: Any) {
+        
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a directory"
+        dialog.showsResizeIndicator    = true
+        dialog.showsHiddenFiles        = false
+        dialog.canChooseDirectories    = true
+        dialog.canCreateDirectories    = true
+        dialog.canChooseFiles          = false
+        dialog.allowsMultipleSelection = false
+        dialog.center()
+        dialog.begin { (result) -> Void in
+        if result == NSFileHandlingPanelOKButton {
+            let result = dialog.url // Pathname of the file
+            self.filename.stringValue = result!.path
+        }
+        else { return}
+        }    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
+    @IBAction func connectToGD(_ sender: Any) {
+        
+        //Connect to Drive
+        //self.textField.stringValue=self.textBox.stringValue
+
+        //if connected then run second window
+        //self.presentViewController(resultViewController, animated:true, completion:nil)        
+    
+    }
+    
+       override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
