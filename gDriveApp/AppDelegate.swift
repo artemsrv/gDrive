dd88ef5a4290f2e0d0318c2aaaffcd17792b65c6
Артем Сидоров <artemsidorov@MacBook-Air-Artem.local>
2016-12-11 21:17:21 +0300

    Added Icons, added sidebar-menu

--- gDriveApp/AppDelegate.swift
+++ gDriveApp/AppDelegate.swift
@@ -11,10 +11,28 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    @IBAction func quitClick(_ sender: NSMenuItem) {
+    
+        //Close Application
+        NSApplication.shared().terminate(self)
+    }
+    
 
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        let ico = NSImage(named:"statusBarIcon")
+        ico?.isTemplate = true //better to dark
+        
+        statusItem.image=ico
+        statusItem.title = "gDrive"
+        statusItem.menu = statusMenu
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
